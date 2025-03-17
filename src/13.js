a9fb17421611e55f97e1fb79194cad2d3af69d89
xuefeichocochoco <xuefeichocochoco@gmail.com>
2025-03-17 18:02:50 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!');
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
