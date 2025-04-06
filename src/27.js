0370b03b7543444756ab723faf1370f7e3bdc870
xuefeichocochoco <xuefeichocochoco@gmail.com>
2025-04-06 13:20:04 +0000

    Add 27.js

---
+++ src/27.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
