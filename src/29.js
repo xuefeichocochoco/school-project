c6ea4e61df0aa62c44a1eb44ef4126fc8786d52e
xuefeichocochoco <xuefeichocochoco@gmail.com>
2025-04-08 17:59:44 +0000

    Add 29.js

---
+++ src/29.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    let previous = 0;
+    let current = 1;
+    for (let i = 2; i < n; i++) {
+      [previous, current] = [current, previous + current];
+    }
+    return current;
+  }
+}
+
+console.log(fibonacci(10)); // Output: 55
